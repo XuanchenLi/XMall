37915090800fca40116a95e6e417f51bb8605e7e
Dazz1e <2468691031@qq.com>
2021-11-29 11:12:00 +0800

    创建服务端项目

---
+++ xmallserver/src/main/java/com/xmall/xmall/XmallApplication.java
@@ -0,0 +1,13 @@
+package com.xmall.xmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallApplication.class, args);
+    }
+
+}
---
+++ xmallserver/src/test/java/com/xmall/xmall/XmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmall.xmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
